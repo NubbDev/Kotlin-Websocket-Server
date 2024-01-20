017c2c37359db59d6075ef064931d31670479b96
Gilbert O. <giloe.dev@gmail.com>
2024-01-19 17:41:29 -0700

    added gson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
 
 tasks.test {
